415084ab38d677b6e7be0104bbe1bbfd61a6f1e6
Siddharth Goswami <sidgoswami9891@gmail.com>
2026-05-03 23:31:23 +0530

    Small chance in comment

--- models.py
+++ models.py
@@ -7,4 +7,4 @@ class CloudService(Base):
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String, unique=True, index=True) # e.g., EC2
     default_config = Column(String) # e.g., t3.medium
-    hourly_rate = Column(Float) # e.g., 0.0416
+    hourly_rate = Column(Float) # e.g., .0416
